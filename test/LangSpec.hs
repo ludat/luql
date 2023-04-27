ccbcc80c09c15daef3aa24d3defcbfdefd0c841f
Lucas David Traverso <lucas6246@gmail.com>
2023-04-27 04:57:47 -0300

    Speed up and refactor the tests a bit

--- test/LangSpec.hs
+++ test/LangSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 module LangSpec where
 
 import Data.Function ((&))
@@ -17,13 +18,100 @@ import Data.Map qualified as Map
 import LuQL.Types (QueryExpression(..), LiteralValue(..))
 import LuQL.Runner (SqlRuntimeRow(..))
 
+spec :: Spec
+spec =  aroundAll withDatabase $ doNotRandomiseExecutionOrder $ do
+  itMatchesSnapshot "from model"
+    [__i|
+      from Languages
+    |]
+
+  itMatchesSnapshot "from model assigning a different name"
+    [__i|
+      from Languages as l
+    |]
+
+  itMatchesSnapshot "from model with implicit wheres"
+    [__i|
+      from Customers
+    |]
+
+  itMatchesSnapshot "where with a true condition"
+    [__i|
+      from Languages
+      where true
+    |]
+
+  itMatchesSnapshot "join with a valid model"
+    [__i|
+      from Countries
+      join Cities
+    |]
+
+  itMatchesSnapshot "let a new column with a constant"
+    [__i|
+      from Languages
+      let column = 0
+    |]
+
+  itMatchesSnapshot "group by with a raw query (count)"
+    [__i|
+      from Cities
+      group by city.country_id {
+        let count = `count(*)`
+      }
+    |]
+
 withDatabase :: (PG.Connection -> IO ()) -> IO ()
 withDatabase action = do
   conn <- PG.connectPostgreSQL "postgres://postgres:123456@localhost:5432/postgres"
-  PG.begin conn
   _ <- action conn
-  PG.rollback conn
   PG.close conn
+
+itMatchesSnapshot :: String -> Text -> TestDefM '[PG.Connection]  () ()
+itMatchesSnapshot name program = describe name $ do
+  let parsedProgram :: Either (ParseErrorBundle Text Void) RawQuery
+      parsedProgram =
+        parseQuery program
+
+  it "0_Parser" $
+    goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/0_Parser.golden|] $
+      parsedProgram
+
+  let compiledProgram :: Either [Error] CompiledQuery
+      compiledProgram =
+        parsedProgram
+          & either (error . show) id
+          & compileProgram models
+  it "1_Compiler" $
+    goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/1_Compiler.golden|] $
+      compiledProgram
+
+  let partialSqlQuery :: LuQL.SqlGeneration.PartialQuery
+      partialSqlQuery =
+        compiledProgram
+          & either (error . show) id
+          & unCompiledQuery
+          & LuQL.SqlGeneration.compileStatements
+  it "2_SqlGeneration" $
+    goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/2_SqlGeneration.golden|] $
+      partialSqlQuery
+
+  beforeAllWith (\conn -> do
+      rawSqlQuery <-
+        partialSqlQuery
+          & renderPartialQuery
+          & renderSqlBuilder conn
+      pure rawSqlQuery
+    ) $ do
+    itWithOuter "3_Render" $ \(rawSqlQuery :: PG.Query) -> do
+      pureGoldenByteStringFile [i|test/.golden/Lang/#{name}/3_Render.golden|] $
+        PG.fromQuery rawSqlQuery
+
+    itWithAll "4_Run" (\(HCons rawSqlQuery (HCons conn HNil) :: HList '[PG.Query, PG.Connection]) () -> do
+      queryResults <- PG.query_ @SqlRuntimeRow conn rawSqlQuery
+      pure $ goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/4_Run.golden|] $
+        queryResults
+      )
 
 models :: Map Text ModelDefinition
 models =
@@ -125,92 +213,3 @@ models =
               [
               ]
         }
-
-itMatchesSnapshot :: String -> Text -> TestDefM outers () ()
-itMatchesSnapshot name program = withoutRetries $ doNotRandomiseExecutionOrder $ sequential $ describe name $ do
-  let parsedProgram :: Either (ParseErrorBundle Text Void) RawQuery
-      parsedProgram =
-        parseQuery program
-
-  it "0_Parser" $
-    goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/0_Parser.golden|] $
-      parsedProgram
-
-  let compiledProgram :: Either [Error] CompiledQuery
-      compiledProgram =
-        parsedProgram
-          & either (error . show) id
-          & compileProgram models
-  it "1_Compiler" $
-    goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/1_Compiler.golden|] $
-      compiledProgram
-
-  let partialSqlQuery :: LuQL.SqlGeneration.PartialQuery
-      partialSqlQuery =
-        compiledProgram
-          & either (error . show) id
-          & unCompiledQuery
-          & LuQL.SqlGeneration.compileStatements
-  it "2_SqlGeneration" $
-    goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/2_SqlGeneration.golden|] $
-      partialSqlQuery
-
-  around withDatabase $ do
-    it "3_Render" $ \conn -> do
-      rawSqlQuery <-
-        partialSqlQuery
-          & renderPartialQuery
-          & renderSqlBuilder conn
-          & fmap PG.fromQuery
-      pure $ pureGoldenByteStringFile [i|test/.golden/Lang/#{name}/3_Render.golden|] $
-        rawSqlQuery
-    it "4_Run" $ \conn -> do
-      queryResults <-
-        partialSqlQuery
-          & renderPartialQuery
-          & renderSqlBuilder conn
-      r <- PG.query_ @SqlRuntimeRow conn queryResults
-      pure $ goldenPrettyShowInstance [i|test/.golden/Lang/#{name}/4_Run.golden|] $ r
-
-spec :: Spec
-spec = doNotRandomiseExecutionOrder $ do
-  itMatchesSnapshot "from model"
-    [__i|
-      from Languages
-    |]
-
-  itMatchesSnapshot "from model assigning a different name"
-    [__i|
-      from Languages as l
-    |]
-
-  itMatchesSnapshot "from model with implicit wheres"
-    [__i|
-      from Customers
-    |]
-
-  itMatchesSnapshot "where with a true condition"
-    [__i|
-      from Languages
-      where true
-    |]
-
-  itMatchesSnapshot "join with a valid model"
-    [__i|
-      from Countries
-      join Cities
-    |]
-
-  itMatchesSnapshot "let a new column with a constant"
-    [__i|
-      from Languages
-      let column = 0
-    |]
-
-  itMatchesSnapshot "group by with a raw query (count)"
-    [__i|
-      from Cities
-      group by city.country_id {
-        let count = `count(*)`
-      }
-    |]
