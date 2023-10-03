34811968e912f2734fd1fccdf3b0d29bbc6d3c84
Lucas David Traverso <lucas6246@gmail.com>
2023-10-03 02:25:13 -0300

    Fix tests

--- test/RunTests.hs
+++ test/RunTests.hs
@@ -1,16 +1,7 @@
 module Main where
 
 import Spec
-
 import Test.Syd
 
 main :: IO ()
-main = do
-  -- sydTest spec
-
-  sydTestWith defaultSettings
-    { settingRetries = 0
-    -- , settingFilters = ["FOCUS"]
-    , settingGoldenReset = True
-    , settingGoldenStart = True
-    } spec
+main = sydTest spec
